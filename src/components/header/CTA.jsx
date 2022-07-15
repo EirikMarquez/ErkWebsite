bae19d1d30de95aa063710d29e5b0bdc04ae88e2
EirikMarquez <eiriklmarquez@gmail.com>
2022-07-15 15:50:07 -0500

    cleaned up files

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import CV from "../../assets/cv.pdf";
+import Resume from "../../assets/EirikResume.pdf";
 const CTA = () => {
   return (
     <div className="cta">
-      <a href={CV} download className="btn">
+      <a href={Resume} download className="btn">
         Download CV
       </a>
       <a href="#contact" className="btn btn-primary">
