77bbe33806a59af6fde8e6af0bdffc0aa110a255
EirikMarquez <109366599+EirikMarquez@users.noreply.github.com>
2023-07-31 18:47:45 -0500

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./header.css";
 import CTA from "./CTA";
-import ME from "../../assets/me.png";
+import ME from "../../assets/Eirik_AM_Picture-removebg-preview.png";
 import HeaderSocial from "./HeaderSocials";
 
 const Header = () => {
